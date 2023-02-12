4a692c105eeb8c19c012d39ef1e84ea5ea913b22
aouaiti <mr.aouaiti.ahmed@gmail.com>
2023-02-12 17:09:39 +0100

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,16 @@ import * as THREE from "three";
 import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
 import { Canvas, useThree } from "@react-three/fiber";
 
-function Shape({ getScene }) {
+function Shape({ container }) {
   const three = useThree();
-  getScene(three);
-  return (
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
-  );
-}
-
-function App() {
-  const container = useRef(null);
-  const [ar, setAr] = useState(null);
-  const [scene, setScene] = useState(null);
-
+  const group = useRef(null);
+  // console.log(container);
+  // console.log(three.scene);
   async function init() {
     return new Promise((resolve, reject) => {
       resolve(
         new MindARThree({
-          container: container.current,
+          container: container,
           imageTargetSrc:
             "https://cdn.jsdelivr.net/npm/mind-ar@1.1.4/examples/image-tracking/assets/card-example/card.mind",
         })
@@ -32,9 +21,41 @@ function App() {
     });
   }
 
-  const getScene = (prop) => {
-    setScene(prop.scene);
-  };
+  useEffect(() => {
+    if (!group) return;
+    console.log(group);
+    init()
+      .then(async (data) => {
+        // console.log(data);
+        const { camera } = data;
+        // console.log(renderer, scene, camera);
+        const anchor = data.addAnchor(0);
+        anchor.group.add(group.current);
+        await data.start();
+        // console.log(scene);
+        renderer.setAnimationLoop(() => {
+          renderer.render(three.scene, camera);
+        });
+      })
+      .catch((err) => console.log(err));
+  }, [group]);
+
+  return (
+    <group ref={group}>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh>
+        <sphereGeometry />
+        <meshBasicMaterial />
+      </mesh>
+    </group>
+  );
+}
+
+function App() {
+  const [container, setContainer] = useState(null);
 
   useEffect(() => {
     (function () {
@@ -48,38 +69,9 @@ function App() {
     })();
   }, []);
 
-  useEffect(() => {
-    if (!container) return;
-    console.log(scene);
-    init()
-      .then(async (data) => {
-        setAr(data);
-        // console.log(data);
-        const { renderer, camera } = data;
-        // console.log(renderer, scene, camera);
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({
-          color: 0x0000ff,
-          transparent: true,
-          opacity: 0.7,
-        });
-        const plane = new THREE.Mesh(geometry, material);
-        const anchor = data.addAnchor(0);
-        anchor.group.add(plane);
-        await data.start();
-        console.log(scene);
-        renderer.setAnimationLoop(() => {
-          renderer.render(scene, camera);
-        });
-      })
-      .catch((err) => console.log(err));
-  }, [container]);
-
   return (
-    <div style={{ width: "100vw", height: "100vh" }} ref={container}>
-      <Canvas>
-        <Shape getScene={getScene} />
-      </Canvas>
+    <div style={{ width: "100vw", height: "100vh" }} ref={setContainer}>
+      <Canvas>{container && <Shape container={container} />}</Canvas>
     </div>
   );
 }
